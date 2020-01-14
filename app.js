8bd6612b9886a7341dfa51f773c64e46b71d9095
Connor <ccovington439@gmail.com>
2020-01-14 16:23:23 -0600

    complete steps 8 and 9

--- app.js
+++ app.js
@@ -9,3 +9,39 @@ function sayHello() {
 };
 
 sayHello();
+
+
+
+// charles is 21
+// abby is 27
+// james is 18
+// john is 17
+// var Charles = 21;
+// var Abby = 27;
+// var James = 18;
+// var John = 17;
+
+let people = [
+    {name: 'Charles', age: 21},
+    {name: 'Abby', age: 27},
+    {name: 'James', age: 18},
+    {name: 'John', age: 17}
+]
+
+function checkAge() {
+    for (i = 0; i < people.length; i++) {
+        if (people[i].age < 21) {
+            alert('Sorry ' + people[i].name + ', you are too young!');
+        } else {
+            alert('Welcome ' + people[i].name + ' to the party!');
+        }
+    }
+}
+
+checkAge();
+
+var veg = ["Black-eyed Peas", "Green beans", "Corn", "Spinach"];
+
+for(var i = 0; i < veg.length; i++) {
+    console.log(veg[i]);
+}
