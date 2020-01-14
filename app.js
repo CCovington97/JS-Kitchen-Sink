3fe45412899a3aed8577f503bae1b57f3aeed779
Connor <ccovington439@gmail.com>
2020-01-14 15:41:51 -0600

    completed steps 1 through 7

---
+++ app.js
@@ -0,0 +1,11 @@
+var firstName = "Connor"
+const states = "50"
+var sum = 5 + 4;
+// adds 5 and 4 together to get 9
+console.log(sum);
+
+function sayHello() {
+    alert("Hello World!");
+};
+
+sayHello();
